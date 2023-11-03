3ed4abe8c5e389c7c221dd1cdf8752477c54f4c9
Elad Nava <eladnava@gmail.com>
2023-11-03 18:09:23 +0900

    Recent Alerts: Display relative alert time ago

--- RedAlert/DateFormatterStruct.swift
+++ RedAlert/DateFormatterStruct.swift
@@ -10,13 +10,38 @@ import Foundation
 
 struct DateFormatterStruct {
     static func ConvertUnixTimestampToDateTime(unixTimestamp: Double!) -> String {
-        // Prepare date formatter for time of alert        
+        // Convert Unix timestamp to Date() object
+        let date = Date(timeIntervalSince1970: TimeInterval(unixTimestamp))
+
+        // Prepare date formatter for generating alert time string
         let dateFormatter = DateFormatter()
         
-        // Set format (13:30:01)        
-        dateFormatter.dateFormat = "(dd/MM/yy) HH:mm:ss"
-        
-        // Convert unix timestamp to readable human time and override timestamp        
-        return dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(unixTimestamp )) as Date)
+        // iOS 13+ required to access RelativeDateTimeFormatter
+        if #available(iOS 13.0, *) {
+            // Initialize formatter
+            let relativeFormatter = RelativeDateTimeFormatter()
+
+            // Calculate relative time ago
+            let relativeDate = relativeFormatter.localizedString(for: date, relativeTo: Date())
+            
+            // Set hours and minutes format (13:30:01)
+            dateFormatter.dateFormat = "(HH:mm:ss)"
+            
+            // Convert unix timestamp to string using format above
+            let time = dateFormatter.string(from: date);
+
+            // Return relative date
+            return relativeDate + " " + time
+        } else {
+            // Set hours and minutes format without parenthesis
+            dateFormatter.dateFormat = "HH:mm:ss"
+            
+            // Convert unix timestamp to string using format above
+            let time = dateFormatter.string(from: date);
+            
+            // Fallback to just displaying the hours and minutes on earlier iOS versions
+            return time
+        }
+
     }
 }
